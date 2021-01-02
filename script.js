c101bd5d08b25d645dc27bcde54be295e51bcf90
Manjinder <manjindersinghjohal@aol.com>
2021-01-03 00:24:01 +0530

    added plus functionality

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const display = document.getElementById('display');
+let number1 = 0;
+let number2 = 0;
+
 const updateDisplay = (number) => {
-    document.getElementById('display').innerHTML = number;
+    display.innerHTML = display.innerHTML + number;
+}
+
+const clearDisplay = () => {
+    display.innerHTML = 0;
+}
+
+const onPlusClick = () => {
+    number1 = display.innerText;
+    clearDisplay();
+}
+
+const onEqualClick = () => {
+    number2 = display.innerText;
+    const result = parseInt(number1) + parseInt(number2);
+    clearDisplay();
+    updateDisplay(result);
 }
